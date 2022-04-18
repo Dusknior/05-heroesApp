1304d0bcb229c5da599a705f5b32d80778d2c06b
Daniel Bernal Herrera <danielbernalherrera@gmail.com>
2022-04-18 09:02:50 -0500

    fourth commit

--- src/app/heroes/pages/home/home.component.ts
+++ src/app/heroes/pages/home/home.component.ts
@@ -1,19 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../../auth/services/auth.service';
+import { Auth } from '../../../auth/interfaces/auth.interface';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styles: [`
-    .container {
-      margin: 10px;
-    }
-  `]
+  styles: [
+    `
+      .container {
+        margin: 10px;
+      }
+    `,
+  ],
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  get auth() {
+    return this.authService.auth;
   }
 
+  constructor(private router: Router, private authService: AuthService) {}
+
+  ngOnInit(): void {}
+
+  logout() {
+    this.router.navigate(['./auth']);
+  }
 }
